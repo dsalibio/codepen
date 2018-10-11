85222c913e74ae833d640e61265318523083512f
Bianca Salunga <biancasalunga@Bangenge-mbp.local>
2018-10-11 11:38:47 +0800

    Updated routing files

--- source/js/constants/indexRoutes.js
+++ source/js/constants/indexRoutes.js
@@ -1,7 +1,9 @@
 import Home from 'views/Home';
 
 const indexRoutes = [
-  { path: '/', name: 'Home Page', component: Home },
+  {
+    path: '/', name: 'Home Page', component: Home, secured: false,
+  },
 ];
 
 export default indexRoutes;
